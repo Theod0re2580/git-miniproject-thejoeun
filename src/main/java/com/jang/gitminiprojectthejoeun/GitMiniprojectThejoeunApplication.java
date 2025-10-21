9dec7551d34a546ec3064c711f09f87b15a4fea5
XXXGHOST <kkb04024@gmail.com>
2025-10-21 17:46:01 +0900

    commit

---
+++ src/main/java/com/jang/gitminiprojectthejoeun/GitMiniprojectThejoeunApplication.java
@@ -0,0 +1,13 @@
+package com.jang.gitminiprojectthejoeun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitMiniprojectThejoeunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitMiniprojectThejoeunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jang/gitminiprojectthejoeun/GitMiniprojectThejoeunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jang.gitminiprojectthejoeun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitMiniprojectThejoeunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
